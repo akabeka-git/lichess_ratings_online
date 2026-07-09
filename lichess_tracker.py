af5ec650bddd3bca1a894981db36de3125f44682
Fieps3000 <fieps3000@fieps3000.thinkpad>
2026-07-09 11:16:02 +0200

    fix: einheitliche breite für alle- und stabil-seite

--- lichess_tracker.py
+++ lichess_tracker.py
@@ -385,6 +385,7 @@ def generate_html(players_data, color_stats=None, page_variant="alle"):
     display: inline-block;
     text-align: left;
     max-width: 100vw;
+    min-width: 580px;
   }}
   h1 {{
     font-size: 19px;
@@ -426,6 +427,7 @@ def generate_html(players_data, color_stats=None, page_variant="alle"):
     .wrapper {{
       transform: none;
       max-width: calc(100vw - 1rem);
+      min-width: calc(100vw - 1rem);
       overflow-x: hidden;
     }}
     h1 {{
